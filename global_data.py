ccb84159368b92927f010b366dcb93744e6fff30
KareemMoataz02 <kareemmoataz13@gmail.com>
2025-02-04 15:30:38 +0200

    GUI updates

---
+++ global_data.py
@@ -0,0 +1,7 @@
+# global_data.py
+"""
+This module holds global variables used across the application.
+For example, global_uploaded_bytes stores the parsed byte data from a hex file upload.
+"""
+
+global_uploaded_bytes = []
